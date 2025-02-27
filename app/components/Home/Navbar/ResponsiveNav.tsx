206b3a748eaa56db3a8231049def947408f9d918
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-27 22:20:06 +0300

    create navbar for web and mobile

---
+++ app/components/Home/Navbar/ResponsiveNav.tsx
@@ -0,0 +1,11 @@
+import { MobileNavbar } from "./MobileNav";
+import { Navbar } from "./nav";
+
+export const ResponsiveNavbar = () => {
+  return (
+    <div>
+      <Navbar />
+      <MobileNavbar />
+    </div>
+  );
+};
