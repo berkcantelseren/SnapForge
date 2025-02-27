3ac8491f1d8763cb49b058cb950c43ba4c9be5d4
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-27 22:15:56 +0300

    create components folder with home and use it on main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Home } from "./components/Home/Home";
 
 const HomePage = () => {
-  return <div className="text-red-400 text-4xl underline">HomePage</div>;
+  return (
+    <div className="text-red-400 text-4xl underline">
+      <Home />
+    </div>
+  );
 };
 
 export default HomePage;
