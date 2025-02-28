d0633adcac4218e596ff85f1b18a09aa6f7c62bf
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-28 15:27:54 +0300

    add closing icon for hamburger and make it responsive for all devices

--- app/components/Home/Navbar/MobileNav.tsx
+++ app/components/Home/Navbar/MobileNav.tsx
@@ -1,5 +1,6 @@
 import { navLinks } from "@/constant/constant";
 import Link from "next/link";
+import { CgClose } from "react-icons/cg";
 
 export const MobileNavbar = () => {
   return (
@@ -11,10 +12,14 @@ export const MobileNavbar = () => {
         {navLinks.map((link) => {
           return (
             <Link key={link.id} href={link.url}>
-              <p>{link.label}</p>
+              <p className="text-white w-fit text-[20px] m1-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
+                {link.label}
+              </p>
             </Link>
           );
         })}
+        {/* Closing Icon For Hamburger */}
+        <CgClose className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6" />
       </div>
     </div>
   );
