03d5af0286418d597e5076d84f8d213a4851190f
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-28 22:35:46 +0300

    create about page

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,9 +1,11 @@
+import { About } from "./About/About";
 import { Hero } from "./Hero/Hero";
 
 export const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <About />
     </div>
   );
 };
