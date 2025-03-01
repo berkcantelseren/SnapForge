7753ba120abd4016a272a27f0ba794964cbff4aa
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-02 02:45:42 +0300

    finished services headings and icons

--- app/components/Home/Services/Services.tsx
+++ app/components/Home/Services/Services.tsx
@@ -1,5 +1,11 @@
-import { FaRocket } from "react-icons/fa6";
+import { FaRocket, FaTwitch, FaUserClock } from "react-icons/fa6";
 import BoxText from "../../Helper/BoxText";
+import {
+  MdBusinessCenter,
+  MdOutlineContactSupport,
+  MdOutlineSupportAgent,
+} from "react-icons/md";
+import { BsBuildingsFill } from "react-icons/bs";
 
 export const Services = () => {
   return (
@@ -34,75 +40,77 @@ export const Services = () => {
               </p>
             </div>
           </div>
-          {/* 1st Service Section  */}
+          {/* 2st Service Section  */}
           <div className="flex items-center space-x-5">
             {/* Icon Section  */}
             <div className="w-14 h-14 min-h-[3rem] min-w-[3.5rem] rounded-md flex items-center justify-center flex-col bg-[#9B2D2F]">
-              <FaRocket className="text-white w-6 h-6" />
+              <FaUserClock className="text-white w-6 h-6" />
             </div>
             {/* Text Section  */}
             <div>
-              <p className="text-lg font-bold text-gray-800">Start Up</p>
+              <p className="text-lg font-bold text-gray-800">
+                Full-Time Access
+              </p>
               <p className="text-sm text-gray-700">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Inventore, nostrum perspiciatis?
               </p>
             </div>
           </div>
-          {/* 1st Service Section  */}
+          {/* 3st Service Section  */}
           <div className="flex items-center space-x-5">
             {/* Icon Section  */}
             <div className="w-14 h-14 min-h-[3rem] min-w-[3.5rem] rounded-md flex items-center justify-center flex-col bg-[#9B2D2F]">
-              <FaRocket className="text-white w-6 h-6" />
+              <BsBuildingsFill className="text-white w-6 h-6" />
             </div>
             {/* Text Section  */}
             <div>
-              <p className="text-lg font-bold text-gray-800">Start Up</p>
+              <p className="text-lg font-bold text-gray-800">Agencies</p>
               <p className="text-sm text-gray-700">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Inventore, nostrum perspiciatis?
               </p>
             </div>
           </div>
-          {/* 1st Service Section  */}
+          {/* 4st Service Section  */}
           <div className="flex items-center space-x-5">
             {/* Icon Section  */}
             <div className="w-14 h-14 min-h-[3rem] min-w-[3.5rem] rounded-md flex items-center justify-center flex-col bg-[#9B2D2F]">
-              <FaRocket className="text-white w-6 h-6" />
+              <MdBusinessCenter className="text-white w-6 h-6" />
             </div>
             {/* Text Section  */}
             <div>
-              <p className="text-lg font-bold text-gray-800">Start Up</p>
+              <p className="text-lg font-bold text-gray-800">Business</p>
               <p className="text-sm text-gray-700">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Inventore, nostrum perspiciatis?
               </p>
             </div>
           </div>
-          {/* 1st Service Section  */}
+          {/* 5st Service Section  */}
           <div className="flex items-center space-x-5">
             {/* Icon Section  */}
             <div className="w-14 h-14 min-h-[3rem] min-w-[3.5rem] rounded-md flex items-center justify-center flex-col bg-[#9B2D2F]">
-              <FaRocket className="text-white w-6 h-6" />
+              <MdOutlineContactSupport className="text-white w-6 h-6" />
             </div>
             {/* Text Section  */}
             <div>
-              <p className="text-lg font-bold text-gray-800">Start Up</p>
+              <p className="text-lg font-bold text-gray-800">7/24 Support</p>
               <p className="text-sm text-gray-700">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Inventore, nostrum perspiciatis?
               </p>
             </div>
           </div>
-          {/* 1st Service Section  */}
+          {/* 6st Service Section  */}
           <div className="flex items-center space-x-5">
             {/* Icon Section  */}
             <div className="w-14 h-14 min-h-[3rem] min-w-[3.5rem] rounded-md flex items-center justify-center flex-col bg-[#9B2D2F]">
-              <FaRocket className="text-white w-6 h-6" />
+              <FaTwitch className="text-white w-6 h-6" />
             </div>
             {/* Text Section  */}
             <div>
-              <p className="text-lg font-bold text-gray-800">Start Up</p>
+              <p className="text-lg font-bold text-gray-800">Streamers</p>
               <p className="text-sm text-gray-700">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Inventore, nostrum perspiciatis?
