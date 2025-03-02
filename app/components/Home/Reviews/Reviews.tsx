29c75d54205ae8eee4617626ccd5051da9a4dc7b
Berkcan Telseren <berkcantelseren@gmail.com>
2025-03-02 22:16:16 +0300

    created heading and desc. section for reviews

--- app/components/Home/Reviews/Reviews.tsx
+++ app/components/Home/Reviews/Reviews.tsx
@@ -1,3 +1,14 @@
 export const Reviews = () => {
-  return <div>Reviews part</div>;
+  return (
+    <div className="pt-16 pb-16 bg-[#f4f4f4]">
+      <h1 className="text-center text-2xl font-bold text-gray-800 custom-selection-3">
+        See What Our Clients Say
+      </h1>
+      <p className="mt-3 text-center font-medium text-gray-700 w-[90%] sm:w-[70%] md:w-[50%] mx-auto">
+        Learn how businesses of all sizes streamline their processes, reduce
+        workload, and achieve growth with SnapForge.
+      </p>
+      {/* Grid Section  */}
+    </div>
+  );
 };
